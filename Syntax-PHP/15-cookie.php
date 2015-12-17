bb639e7c589bd670961e35f4f1d3f94c81184aa3
Fabrício José <faesouza@outlook.com>
2015-12-16 22:43:28 -0200

    rm cookie

--- Syntax-PHP/15-cookie.php
+++ Syntax-PHP/15-cookie.php
@@ -8,3 +8,5 @@
 		// nome do cookie; valor; prazo de validade; path; domínio; https; httpOnly
 		setcookie('chave', 'valor', time()+60, '/', 'php-dev.tilibra.com.br', false, true);
 	}
+
+	setcookie('__utmz', '', time()-1);
